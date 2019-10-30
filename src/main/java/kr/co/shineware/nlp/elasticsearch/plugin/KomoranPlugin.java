31a137afa5fdfd622d0b310f18c06ffcd862d36f
junsoo shin <jsshin@shineware.co.kr>
2019-10-31 01:55:06 +0900

    apply gitignore

--- src/main/java/kr/co/shineware/nlp/elasticsearch/plugin/KomoranPlugin.java
+++ src/main/java/kr/co/shineware/nlp/elasticsearch/plugin/KomoranPlugin.java
@@ -4,13 +4,14 @@ import kr.co.shineware.nlp.elasticsearch.index.KomoranTokenizerFactory;
 import org.elasticsearch.index.analysis.TokenizerFactory;
 import org.elasticsearch.indices.analysis.AnalysisModule;
 import org.elasticsearch.plugins.AnalysisPlugin;
+import org.elasticsearch.plugins.Plugin;
 
 import java.util.Map;
 
 import static java.util.Collections.emptyMap;
 import static java.util.Collections.singletonMap;
 
-public class KomoranPlugin implements AnalysisPlugin {
+public class KomoranPlugin extends Plugin implements AnalysisPlugin {
 
     @Override
     public Map<String, AnalysisModule.AnalysisProvider<TokenizerFactory>> getTokenizers() {
